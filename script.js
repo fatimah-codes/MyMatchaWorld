81527db0eec13e099596aa4e5d43ded8a7a52b36
Fatimah Albariqi <fatemahh744@gmail.com>
2025-11-22 14:29:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+window.onload = function() {
+    console.log('Page loaded');
+};
+const images = document.querySelectorAll('img');
+images.forEach((img, index) => {
+    img.setAttribute('tabindex', index + 1);
+});
+function highlight(element) {
+    element.style.border = '3px solid green';
+    console.log('Image focused');
+}
+function removeHighlight(element) {
+    element.style.border = 'none';
+}
